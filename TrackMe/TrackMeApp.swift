22cdba36af7dbed6d90ed458fd977bce2ee9f660
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-07-29 21:44:24 +0200

    Initial commit

---
+++ TrackMe/TrackMeApp.swift
@@ -0,0 +1,20 @@
+//
+//  TrackMeApp.swift
+//  TrackMe
+//
+//  Created by Leander Van Aarde on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrackMeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
